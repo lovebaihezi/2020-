de0eae1d994321126d18f47609be24342f05c9c0
lovebaihezi <x18739168862@gmail.com>
2020-08-04 10:57:48 +0800

    Unimportant thing

---
+++ 暑期任务/NoteBooks/Js/NoteBook.js
@@ -0,0 +1,26 @@
+function FlexBox(number, status, node) {
+    this.Number = number;
+    this.Status = status;
+    this.Node = Node;
+}
+FlexBox.prototype.SetNumber = function(PremierOfThisElement) {
+    if (PremierOfThisElement == NULL) {
+        this.Number = 1;
+    } else {
+        this.Number = PremierOfThisElement.Number + 1;
+    }
+}
+
+FlexBox.prototype.SwitchToTextArea = function() {
+    FlexBox.
+}
+
+FlexBox.prototype.NodeList = NodeListGet();
+
+FlexBox.prototype.NodeListGet = function() {
+
+}
+
+window.onload = function() {
+    var Number = document.getElementsByClassName("flex").length;
+}
