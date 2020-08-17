6cb7ac4182f79385e2342fd89db1c23939de8971
lovebaihezi <x18739168862@gmail.com>
2020-08-17 11:26:34 +0800

    Add features

--- 暑期任务/mission1/mission1.js
+++ 暑期任务/mission1/mission1.js
@@ -29,10 +29,40 @@ Picture.prototype = {
     }
 }
 
+function Transfer(Ele, Num) {
+    this.Element = Ele;
+    this.Number = Num;
+}
+
+Transfer.prototype = {
+    constructor: Transfer,
+    Alive: function() {
+        var that = this;
+        that.Element.addEventListener("click", function() {
+            PictureCount = that.Number;
+            that.clear();
+            that.show();
+            PictureListGet[0].style.margin = PictureList[0].moveAction(PictureCount);
+        });
+    },
+    clear: function() {
+        for (i = 0; i < RoundListGet.length; i++) {
+            (function(i) {
+                RoundList[i].Element.style.background = "snow";
+            })(i);
+        }
+    },
+    show: function() {
+        this.clear();
+        this.Element.style.background = "yellow";
+    }
+}
+
 function SwitchButton(Element) {
     this.LeftEle = Element[0];
     this.RightEle = Element[1];
 }
+
 SwitchButton.prototype = {
     constructor: SwitchButton,
     Right: function() {
@@ -42,6 +72,7 @@ SwitchButton.prototype = {
                 PictureCount = 0;
             }
             PictureListGet[0].style.margin = PictureList[0].moveAction(PictureCount);
+            RoundList[PictureCount].show();
             if (PictureCount == PictureListGet.length) {
                 PictureListGet[0].style.margin = PictureList[0].moveAction(PictureCount = 0);
             }
@@ -50,26 +81,36 @@ SwitchButton.prototype = {
     Left: function() {
         this.LeftEle.onclick = function() {
             PictureCount--;
-            if (PictureCount <= 0) {
-                PictureCount = 0;
+            if (PictureCount < 0) {
+                PictureCount = PictureListGet.length - 1;
             }
             PictureListGet[0].style.margin = PictureList[0].moveAction(PictureCount);
+            RoundList[PictureCount].show();
         }
     }
 }
 
 var PictureList = new Array();
+var RoundList = new Array();
 var PictureCount = 0;
 var PictureListGet = document.getElementsByClassName("SwitchPicture");
+var RoundListGet = document.getElementsByClassName("round");
 var SwitchButtonList = new SwitchButton(document.getElementsByClassName("button"));
 SwitchButtonList.Left();
 SwitchButtonList.Right();
 for (i = 0; i < PictureListGet.length; i++) {
-    PictureList.push(new Picture("", (function(i) {
-        return PictureListGet[i];
-    })(i), "off"));
+    PictureList.push(new Picture("", (function(i) { return PictureListGet[i]; })(i), "off"));
+    RoundList.push(
+        new Transfer(
+            (function(i) { return RoundListGet[i]; })(i),
+            (function(i) { return i; })(i)
+        )
+    );
     (function(i) {
         PictureList[i].IntervalStop();
+    })(i);
+    (function(i) {
+        RoundList[i].Alive();
     })(i);
 }
 
@@ -78,6 +119,6 @@ var TimeSwitch;
 function IntervalSwitch() {
     return window.setInterval(function() {
         SwitchButtonList.RightEle.click();
-    }, 20000000);
+    }, 3000);
 }
 TimeSwitch = IntervalSwitch();
